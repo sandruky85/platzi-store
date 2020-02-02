2cbb6c46f1bb8825e31a64b73b5e2515913e39d6
sandra.menendez <sandra.menendez@rakuten.de>
2020-02-02 19:18:30 +0100

    Simulate click

---
+++ src/__mocks__/ProductMock.js
@@ -0,0 +1,9 @@
+const Product = {
+  id: '1',
+  image: 'https://arepa.s3.amazonaws.com/camiseta.png',
+  title: 'Camiseta',
+  price: 25,
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+};
+
+export default Product;
---
+++ src/__test__/components/Product.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import ProductMock from '../../__mocks__/ProductMock';
+import Product from '../../components/Product';
+
+describe('<Product />', () => {
+  test('Render component ', () => {
+    const product = shallow(
+      <ProviderMock>
+        <Product />
+      </ProviderMock>,
+    );
+    expect(product.length).toEqual(1);
+  });
+
+  test('Testing add to card button ', () => {
+    const handleAddToCart = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product
+          product={ProductMock}
+          handleAddToCart={handleAddToCart}
+        />
+      </ProviderMock>,
+    );
+    wrapper.find('button').simulate('click');
+    expect(handleAddToCart).toHaveBeenCalledTimes(1);
+  });
+
+});
