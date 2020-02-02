62c5ae2f208d1f30a74159a9c3b254ba21af85fa
sandra.menendez <sandra.menendez@rakuten.de>
2020-02-02 18:46:46 +0100

    Implementing Providers

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Footer from '../../components/Footer';
+import ProviderMock from '../../__mocks__/ProviderMock';
 
 describe('<Footer />', () => {
-  const footer = mount(<Footer />);
-
   test('Rendering Footer component ', () => {
+    const footer = shallow(
+      <ProviderMock>
+        <Footer />
+      </ProviderMock>,
+    );
     expect(footer.length).toEqual(1);
   });
   test('Rendering title', () => {
+    const footer = mount(
+      <ProviderMock>
+        <Footer />
+      </ProviderMock>,
+    );
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
